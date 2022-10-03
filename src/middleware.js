7cad092e40e14b3a868ecc2cdf5d4bdf3659b464
Sijun <sionwer4@naver.com>
2022-10-03 19:18:29 +0900

    sitename for orangetube

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import app from "./server";
 
 export const localMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
-    res.locals.siteName = "Wetube";
+    res.locals.siteName = "Orangetube";
     res.locals.loggedInUser = req.session.user;
     next();
 }
