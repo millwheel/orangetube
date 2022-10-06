99d4ccf69d46757a899e84e3ebb1f2ae7d09d207
Sijun <sionwer4@naver.com>
2022-10-06 19:01:41 +0900

    cross origin middleware add

--- src/middleware.js
+++ src/middleware.js
@@ -57,6 +57,8 @@ export const videoUpload = multer({
 
 export const crossOrigin = (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
