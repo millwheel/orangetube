c9c416d17359267e035a18d61ecb0c7fc57f0836
Sijun <sionwer4@naver.com>
2023-03-30 16:15:00 +0900

    feat:Add mongoose config

--- src/db.js
+++ src/db.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    userFindAndModify: false,
+    useCreateIndex: true,
+});
 
 const db = mongoose.connection;
 
