59fc6db2b75466b7e2bddd9563bbc382e916e6cb
Sijun <sionwer4@naver.com>
2023-04-08 22:45:53 +0900

    fix: eliminate /image and /video in multerS3

--- src/middleware.js
+++ src/middleware.js
@@ -13,13 +13,13 @@ const prod = 1;
 
 const s3ImageUploader = multerS3({
     s3: s3,
-    bucket: "orangetube/images",
+    bucket: "orangetube",
     ACL: 'public-read-write',
 });
 
 const s3VideoUploader = multerS3({
     s3: s3,
-    bucket: "orangetube/videos",
+    bucket: "orangetube",
     ACL: 'public-read-write',
 });
 
