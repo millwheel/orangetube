0a576892585e490c892b5c0bbf4f16111f24fe94
Sijun <sionwer4@naver.com>
2023-04-12 12:15:01 +0900

    fix: Update aws-sdk dependency

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,11 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
-import {S3Client} from "@aws-sdk/client-s3";
+import aws from "aws-sdk";
 
-const s3 = new S3Client({
-    region:"ap-northeast-2",
-    credentials: {
-        accessKeyId: process.env.AWS_ID,
-        secretAccessKey: process.env.AWS_SECRET
-    }
+const s3 = new aws.S3({
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET, 
+    region:"ap-northeast-2"
 });
 
 const prod = 1;
